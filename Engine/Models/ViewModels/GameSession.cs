adb92fbdedc1ad8942457238950dcd96edbdcb65
jlghabon <makilamperouge@gmail.com>
2024-04-15 14:52:59 +0800

    Display player information in view

---
+++ Engine/Models/ViewModels/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer {  get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Vitiate";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+
+        }
+    }
+}
--- Vault/MainWindow.xaml.cs
+++ Vault/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Engine.Models.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,11 @@ namespace Vault;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private GameSession _gameSession;
     public MainWindow()
     {
         InitializeComponent();
+        _gameSession = new GameSession();
+        DataContext = _gameSession;
     }
 }
